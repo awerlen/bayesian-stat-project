a5dc8dd2d183f41ef18ffad37ba37a0bda153642
Sylvain <slambert@student.ethz.ch>
2024-06-11 11:25:57 +0200

    Add runplot to dynasty.py

--- Dynesty.py
+++ Dynesty.py
@@ -1,41 +1,56 @@
-import numpy as np
+import corner
 import dynesty
 import matplotlib.pyplot as plt
-import corner
+import numpy as np
+from dynesty import plotting as dyplot
+from himmelblau as himmelblau
 
-# Define the Himmelblau function
-def himmelblau(x):
-    return (x[0]**2 + x[1] - 11)**2 + (x[0] + x[1]**2 - 7)**2
-# Define the prior transform function
+# define the prior transform function
 def prior_transform(uv):
-    return 10.0 * uv - 4.0  # Transform [0, 1]^2 to [-5, 5]^2
+    return 8.0 * uv - 4.0
 
-# Define the log-likelihood function
+# define the log-likelihood function
 def log_likelihood(x):
-    return -himmelblau(x)  # Since we are minimizing the Himmelblau function
+    return -himmelblau(x)
 
-# Set up the nested sampler
-ndim = 2  # Number of dimensions
-sampler = dynesty.NestedSampler(log_likelihood, prior_transform, ndim)
+# define the number of dimensions
+ndim = 2
 
-# Run the sampler
+# define the number of living points used
+n_live_points = 500
+
+# define the sampler
+sampler = dynesty.DynamicNestedSampler(log_likelihood, prior_transform, ndim, n_live_points)
+
+# run the sampler
 sampler.run_nested()
 results = sampler.results
-
 results.summary()
-# Extract the samples and weights
+
+# extract the samples and weights
 samples = results.samples
 weights = np.exp(results.logwt - results.logz[-1])
 
-# Plot the results
+# plot the results
 plt.figure(figsize=(10, 6))
-plt.scatter(samples[:, 0], samples[:, 1], c=weights, cmap='viridis')
+plt.scatter(samples[:, 0], samples[:, 1], c=weights, cmap='magma')
 plt.colorbar(label='Weight')
 plt.xlabel('x')
 plt.ylabel('y')
-plt.title('Nested Sampling of the Himmelblau Function')
+plt.title('Nested sampling of the Himmelblau function')
 plt.show()
 
 # Plot the corner plot
 corner.corner(samples, weights=weights, labels=['x', 'y'], show_titles=True, title_fmt='.2f')
+plt.show()
+
+# Plot a summary of the run.
+rfig, raxes = dyplot.runplot(results)
+
+# Plot traces and 1-D marginalized posteriors.
+tfig, taxes = dyplot.traceplot(results)
+
+# Plot the 2-D marginalized posteriors.
+cfig, caxes = dyplot.cornerplot(results)
+
 plt.show()
